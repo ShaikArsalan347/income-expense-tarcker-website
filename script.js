3c784183ad3d4834bf00258e64312ef895018413
ShaikArsalan347 <shaikarsalan347@gmail.com>
2025-06-06 15:09:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const toggleBtn = document.querySelector(".toggle-nav");
+const linksContainer = document.querySelector(".links");
+
+const links = document.querySelectorAll(".link");
+toggleBtn.addEventListener("click", () => {
+  linksContainer.classList.toggle("show");
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    linksContainer.classList.remove("show");
+  });
+});
+
+const customerImg = document.querySelector(".customer-img");
+const testimonialText = document.querySelector(".testimonial-text-box");
+const customerName = document.querySelector(".customer-name");
+
+let activeTestimonial = 0;
+
+const updateTestimonial = async function () {
+  const res = await fetch("testimonials.json");
+  const testimonials = await res.json();
+  const { name, profession, img, text } = testimonials[activeTestimonial];
+  testimonialText.innerText = text;
+  customerImg.src = img;
+  customerName.innerText = `${name}, ${profession}`;
+
+  activeTestimonial++;
+
+  if (activeTestimonial > testimonials.length - 1) {
+    activeTestimonial = 0;
+  }
+};
+setInterval(updateTestimonial, 5000);
