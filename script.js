ba44270e9511436064f801748f73613c2d5584ad
ShaikArsalan347 <shaikarsalan347@gmail.com>
2025-06-16 19:33:38 +0530

    .

--- script.js
+++ script.js
@@ -3,33 +3,33 @@ const linksContainer = document.querySelector(".links");
 
 const links = document.querySelectorAll(".link");
 toggleBtn.addEventListener("click", () => {
-  linksContainer.classList.toggle("show");
+  linksContainer.classList.toggle("visible");
 });
 
 links.forEach((link) => {
   link.addEventListener("click", () => {
-    linksContainer.classList.remove("show");
+    linksContainer.classList.remove("visible");
   });
 });
 
-const customerImg = document.querySelector(".customer-img");
-const testimonialText = document.querySelector(".testimonial-text-box");
-const customerName = document.querySelector(".customer-name");
+// const customerImg = document.querySelector(".customer-img");
+// const testimonialText = document.querySelector(".testimonial-text-box");
+// const customerName = document.querySelector(".customer-name");
 
-let activeTestimonial = 0;
+// let activeTestimonial = 0;
 
-const updateTestimonial = async function () {
-  const res = await fetch("testimonials.json");
-  const testimonials = await res.json();
-  const { name, profession, img, text } = testimonials[activeTestimonial];
-  testimonialText.innerText = text;
-  customerImg.src = img;
-  customerName.innerText = `${name}, ${profession}`;
+// const updateTestimonial = async function () {
+//   const res = await fetch("testimonials.json");
+//   const testimonials = await res.json();
+//   const { name, profession, img, text } = testimonials[activeTestimonial];
+//   testimonialText.innerText = text;
+//   customerImg.src = img;
+//   customerName.innerText = `${name}, ${profession}`;
 
-  activeTestimonial++;
+//   activeTestimonial++;
 
-  if (activeTestimonial > testimonials.length - 1) {
-    activeTestimonial = 0;
-  }
-};
-setInterval(updateTestimonial, 5000);
+//   if (activeTestimonial > testimonials.length - 1) {
+//     activeTestimonial = 0;
+//   }
+// };
+// setInterval(updateTestimonial, 5000);
